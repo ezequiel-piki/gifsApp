7e7cefcaccc43e01de14b7c4dd7e8fc606d54068
ezequiel-piki <ezequielpiki23@gmail.com>
2022-05-02 17:31:54 -0300

    fin seccion 7 curso de angular

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,35 +1,60 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-  private apiKey: string = 'Txl9y4sH9Y6hNnnCcjVzvkuwl4dehKnl'
+  private apiKey: string = 'WD0f9sxIZi1i16w8AeguwagehtJhx9GR'
   private _historial : string [] = [] ;
- 
+  private servicioUrl : string = 'https://api.giphy.com/v1/gifs'
+  
+  /* TODO: Cambiar any por su tipo */
+  public resultados : Gif [] = [];
  
  get historial(){
    return [...this._historial];
 
  }
 
+ constructor (private http:HttpClient){
+
+  this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+  
+  this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+ }
+
  buscarGifs(query:string=''){
  
-  query=query.trim().toLowerCase();
+  query = query.trim().toLowerCase();
 
   
   this._historial.unshift(query)
 
-  this._historial = this._historial.splice(0,10)
+  this._historial =  this._historial.splice(0,10)
 
-  this._historial= this.historial.filter((query,index)=>{
+  this._historial =  this.historial.filter((query,index)=>{
     
-     return this.historial.indexOf(query) === index;
+  return this.historial.indexOf(query) === index;
 
   })
+  
+   localStorage.setItem('historial',JSON.stringify(this._historial));
+
+   const params = new HttpParams()
+   .set('api_key',this.apiKey)
+   .set('limit','10')
+   .set('q',query);
 
 
-  console.log(this._historial)
- }
+   this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, {params})
+   .subscribe((resp)=>{
+     this.resultados=resp.data;
+     localStorage.setItem('resultados', JSON.stringify(this.resultados));
+
+
+   })
+}
  
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -16,6 +16,9 @@ export class SidebarComponent {
   return this.gifsService.historial
    }  
 
+   buscar (termino:string){
+   this.gifsService.buscarGifs(termino)
+  }
   
 
 }
